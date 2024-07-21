9ad104fdb2d737371e0439258d696e46eaa5257d
mendoc <ongouadimitri5@gmail.com>
2024-07-21 15:51:31 +0100

    Update response content

--- app.js
+++ app.js
@@ -23,10 +23,10 @@ app.use(pinoHttp);
 // Example endpoint
 app.get('/', async (req, res) => {
   // Use basic logger without HTTP request info
-  logger.info({logField: 'custom-entry', arbitraryField: 'custom-entry'}); // Example of structured logging
+  logger.info({logField: 'origin', arbitraryField: 'local-ubuntu'}); // Example of structured logging
   // Use request-based logger with log correlation
   req.log.info('Child logger with trace Id.'); // https://cloud.google.com/run/docs/logging#correlate-logs
-  res.send('Hello World!');
+  res.send('Mbolo World!');
 });
 
 export default app;
